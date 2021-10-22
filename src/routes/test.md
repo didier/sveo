2e66bbfe3048b2474171bbf518626f943792451d
didier <didierlennon@gmail.com>
2021-10-22 23:11:56 +0200

    Implement mdsvex demo

---
+++ src/routes/test.md
@@ -0,0 +1,5 @@
+---
+title: 'This is the test page.'
+---
+
+<h1>{title}</h1>
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,10 @@
+import { mdsvex } from 'mdsvex'
+import mdsvexConfig from './mdsvex.config.js'
 import preprocess from 'svelte-preprocess'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
@@ -9,7 +13,8 @@ const config = {
 	preprocess: [
 		preprocess({
 			postcss: true
-		})
+		}),
+		mdsvex(mdsvexConfig)
 	]
 }
 
